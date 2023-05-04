3518d93dc6ab40515308be83d0bcbbb9f12736ac
Blooming Burden <flowersoverskulls96@gmail.com>
2023-05-04 11:42:15 +0300

    Small change

--- moveEffects.js
+++ moveEffects.js
@@ -2,7 +2,7 @@ import { throttling } from "./utils.js";
 
 const dataEffects = document.querySelectorAll('[data-move-effects]');
 
-const setStyleOnChildren = (elem, effect) => {
+const setStyleOnChildren = (elem, visible, unvisible) => {
     const tags = ['DIV', 'P', 'H1', 'H2', 'H3', 'H4', 'LI', 'BUTTON', 'A'];
 
     if (elem.children.length === 0) {
@@ -10,27 +10,29 @@ const setStyleOnChildren = (elem, effect) => {
             const elemPosY = window.pageYOffset + elem.getBoundingClientRect().top;
             const currentY = window.pageYOffset + window.innerHeight;
             
-            startMoving(elem, elemPosY, currentY, effect);
+            startMoving(elem, elemPosY, currentY, visible, unvisible);
         } else if (tags.includes(elem.parentElement.tagName)) {
             const elemPosY = window.pageYOffset + elem.parentElement.getBoundingClientRect().top;
             const currentY = window.pageYOffset + window.innerHeight;
             
-            startMoving(elem.parentElement, elemPosY, currentY, effect);
+            startMoving(elem.parentElement, elemPosY, currentY, visible, unvisible);
         }
     }
 
     for (let item of elem.children) {
-        setStyleOnChildren(item, effect);
+        setStyleOnChildren(item, visible, unvisible);
     }
 };
 
-const startMoving = (item, elemPosY, currentY, myClass) => {
+const startMoving = (item, elemPosY, currentY, visible, unvisible) => {
     if (currentY >= elemPosY) {
-        item.classList.add(myClass);
+        item.classList.remove(unvisible);
+        item.classList.add(visible);
     }
 
     if (currentY <= elemPosY - 100) {
-        item.classList.remove(myClass);
+        item.classList.remove(visible);
+        item.classList.add(unvisible);
     }
 }
 
@@ -39,13 +41,14 @@ const setMoveEffects = (evt) => {
         const elemPosY = window.pageYOffset + item.getBoundingClientRect().top;
         const currentY = window.pageYOffset + window.innerHeight;
         const currentEffects = item.dataset.moveEffects.split(',');
-        const child = currentEffects[1] ? currentEffects[1].trim() : false;
-        const myClass = currentEffects[0];
+        const child = currentEffects[2] ? currentEffects[2].trim() : false;
+        const classVisible = currentEffects[0].trim();
+        const classUnvisible = currentEffects[1].trim();
                 
         if (child === 'true') {
-            setStyleOnChildren(item, myClass);
+            setStyleOnChildren(item, classVisible, classUnvisible);
         } else {
-            startMoving(item, elemPosY, currentY, myClass);
+            startMoving(item, elemPosY, currentY, classVisible, classUnvisible);
         }
 
     });
